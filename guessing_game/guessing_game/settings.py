da479594d39a9c303f85198f8fa8e258246aea4c
Stephen <stephen.a.steyaert@gmail.com>
2024-05-13 18:11:47 -0400

    Vercel

--- guessing_game/guessing_game/settings.py
+++ guessing_game/guessing_game/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-707sq(8gdy0fy8a=9&av5ao5llm1-r$m5*uhizsq&q$dsx9np2'
+SECRET_KEY = environ.get('key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
